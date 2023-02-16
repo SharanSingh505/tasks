f16b89bfb4dd457a2f744bd7e136302fcaf2e9a9
Sharanjit <sharansm@udel.edu>
2023-02-15 19:24:36 -0500

    bootstrap,header,image

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Button, Container, Col, Row } from "react-bootstrap";
+//import Container from "react-bootstrap/esm/Container";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,12 +9,30 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <img src={require("./Steph.jpg")} alt="A picture of Steph Curry" />
+            <h1>This is header text</h1>
+            <ul>
+                <li>first element </li>
+                <li>second element</li>
+                <li>third element</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
             <p>Hello World</p>
-            <p>Author: Sharanjit Singh Hello World</p>
+            <p>Author: Sharanjit Singh</p>
+            <Container>
+                <Row>
+                    <Col>First Column.</Col>
+                    <div className="redrectangle"></div>
+                    <Col>Second Column.</Col>
+                    <div className="redrectangle"></div>
+                </Row>
+            </Container>
         </div>
     );
 }
