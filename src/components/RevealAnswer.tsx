fc126670a63328bd2a651892926d3a2c660ffa8a
Sharanjit Singh <sharanjitsingh@sharanjits-mbp.lan>
2023-03-28 11:16:59 -0400

    finished components

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,17 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [isVisible, setVisible] = useState<boolean>(false);
+
+    function flipVisibility(): void {
+        setVisible(!isVisible);
+    }
+
+    return (
+        // reveals answer
+        <div>
+            <Button onClick={flipVisibility}>Reveal Answer</Button>
+            {isVisible && <div>42</div>}
+        </div>
+    );
 }
